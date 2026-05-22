84067c1884e207d58832be83e21f8bb3905d9830
farheenfatima04 <farheenfatima04@outlook.com>
2026-05-22 06:41:01 -0700

    Update helper.py

--- helper.py
+++ helper.py
@@ -41,12 +41,18 @@ def country_year_list(df):
 
     return years,country
 
-def data_over_time(df,col):
+def data_over_time(df, col):
 
-    nations_over_time = df.drop_duplicates(['Year', col])['Year'].value_counts().reset_index().sort_values('index')
-    nations_over_time.rename(columns={'index': 'Edition', 'Year': col}, inplace=True)
+    nations_over_time = (
+        df.drop_duplicates(['Year', col])
+        .groupby('Year')[col]
+        .count()
+        .reset_index()
+    )
+
+    nations_over_time.rename(columns={'Year': 'Edition', col: col}, inplace=True)
+
     return nations_over_time
-
 
 def most_successful(df, sport):
     temp_df = df.dropna(subset=['Medal'])
